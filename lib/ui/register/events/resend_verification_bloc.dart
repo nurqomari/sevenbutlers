7be4cfc8a97e3ae0b215a7eb502247c7285a9dac
nur qomari <nurqomari@gmail.com>
2021-03-05 17:20:02 +0700

    update resend verification bloc

--- lib/ui/register/events/resend_verification_bloc.dart
+++ lib/ui/register/events/resend_verification_bloc.dart
@@ -1,12 +1,8 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sevenbutlers/core/register/register_repository.dart';
-import 'package:sevenbutlers/ui/register/data/register_data.dart';
 import 'package:sevenbutlers/ui/register/data/resend_verification_data.dart';
-import 'package:sevenbutlers/ui/register/events/register_event.dart';
-import 'package:sevenbutlers/ui/register/events/register_state.dart';
 import 'package:sevenbutlers/ui/register/events/resend_verification_event.dart';
 import 'package:sevenbutlers/ui/register/events/resend_verification_state.dart';
-import 'package:sevenbutlers/utils/services/session_manager.dart';
 
 class ResendVerificationBloc
     extends Bloc<ResendVerificationEvent, ResendVerificationState> {
